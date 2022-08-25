d83fb6f72d78fa3654dbafd2de738a12b04d08d1
will0526 <willwyy@163.com>
2022-08-25 16:38:20 +0800

    update 2

---
+++ leetcode/editor/cn/[2]两数相加.py
@@ -0,0 +1,80 @@
+# 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。 
+# 
+#  请你将两个数相加，并以相同形式返回一个表示和的链表。 
+# 
+#  你可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  
+# 输入：l1 = [2,4,3], l2 = [5,6,4]
+# 输出：[7,0,8]
+# 解释：342 + 465 = 807.
+#  
+# 
+#  示例 2： 
+# 
+#  
+# 输入：l1 = [0], l2 = [0]
+# 输出：[0]
+#  
+# 
+#  示例 3： 
+# 
+#  
+# 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# 输出：[8,9,9,9,0,0,0,1]
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  每个链表中的节点数在范围 [1, 100] 内 
+#  0 <= Node.val <= 9 
+#  题目数据保证列表表示的数字不含前导零 
+#  
+#  Related Topics 递归 链表 数学 👍 8538 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        current = ListNode(0)
+        head = current
+        carry = 0  # 负责进位
+        while (l1 or l2):
+            n1 = l1.val if l1 else 0
+            l1 = l1.next if l1 else None
+
+            n2 = l2.val if l2 else 0
+            l2 = l2.next if l2 else None
+
+            sum_val = n1 + n2 + carry
+            current.next = ListNode(sum_val % 10)
+            current = current.next
+            carry = sum_val // 10
+
+        if carry > 0:
+            # 有进位，加 1
+            current.next = ListNode(1)
+        return head.next
+
+# leetcode submit region end(Prohibit modification and deletion)
